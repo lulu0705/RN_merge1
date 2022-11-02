f7eda9d72dfbfa033ed9742430f007118e975400
lulu0705 <86127529+lulu0705@users.noreply.github.com>
2022-11-02 11:22:44 +0800

    Add files via upload

---
+++ Screens/components/Messageboard.js
@@ -0,0 +1,19 @@
+/*仙人掌培育室按留言板切換到這個頁面 */
+import React from "react";
+import {Text, View } from "react-native";
+
+const Messageboard = ({navigation}) => {
+    
+        return(
+            <View>
+                <Text> Messageboard screen</Text>
+                {/* <Button
+                    title="go to Nurturehome"
+                    onPress={()=> this.props.navigation.navigate('CactusCommunicate')}
+                /> */}
+                    
+            </View>
+        );
+    
+}
+export default Messageboard;
